d6d4efa8cd4c8cadf93a0ee538862cf18965458e
chyj4747 <chyj4747@163.com>
2015-11-26 17:56:50 -0500

    nothing

--- App/API/Controller/IndexController.class.php
+++ App/API/Controller/IndexController.class.php
@@ -5,6 +5,9 @@ class IndexController extends RestController {
     protected $allowType = array('json'); // REST允许请求的资源类型列表
 
     public function rest() {
+        $username = I("username");
+        $password = I('password');
+
         switch ($this->_method){
             case 'get':
                 if ($this->_type == 'html') {
@@ -12,6 +15,8 @@ class IndexController extends RestController {
                         "status" => true,
                         "type" => $this->_type,
                         "method" => $this->_method,
+                        "user" => $username,
+                        "pass" => $password,
                         "data" => I("get.")
                     );
                     $this->response($data, "json");
@@ -25,6 +30,8 @@ class IndexController extends RestController {
                         "status" => true,
                         "type" => $this->_type,
                         "method" => $this->_method,
+                        "user" => $username,
+                        "pass" => $password,
                         "data" => I("post.")
                     );
                     $this->response($data, "json");
