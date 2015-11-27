8d560b4ed21eed7b41cde812511f8702ba9a2b45
chyj4747 <chyj4747@163.com>
2015-11-27 14:11:17 -0500

    api update

--- App/API/Controller/BrandController.class.php
+++ App/API/Controller/BrandController.class.php
@@ -1,14 +1,14 @@
 <?php
 namespace API\Controller;
 use Think\Controller\RestController;
-class CategoryController extends RestController {
+class BrandController extends RestController {
     protected $allowType = array('json'); // REST允许请求的资源类型列表
 
     public function basic() {
         $lang = I("lang");
         if ($lang == '') $lang = C("API_DEFAULT_LANG");
         $api = I("api");
-        $db = "category_" . $lang;
+        $db = "brand_" . $lang;
 
         $res = array('status'=>false);
         if ($this->_method == 'post') {
@@ -24,25 +24,28 @@ class CategoryController extends RestController {
                 case 'update':
                     $id = I("id");
                     $name = I("name");
-                    if ($id == '' || $name == '') {
-                        $res['err'] = 'Require "id" and "name"';
+                    $desc = I("desc");
+                    if ($id == '') {
+                        $res['err'] = 'Require "id"';
                     } else {
                         $Model = M($db);
-                        $data = array(
-                            "name" => $name
-                        );
+                        $data = array();
+                        if ($name != '') $data['name'] = $name;
+                        if ($desc != '') $data['desc'] = $desc;
                         if ($Model->where("id=%d", $id)->save($data) !== false) $res['status'] = true;
                         else $res['err'] = 'Update failed';
                     }
                     break;
                 case 'add':
                     $name = I("name");
+                    $desc = I("desc");
                     if ($name == '') {
                         $res['err'] = 'Require "name"';
                     } else {
                         $Model = M($db);
                         $data = array(
-                            "name" => $name
+                            "name" => $name,
+                            "desc" => $desc
                         );
                         if ($Model->add($data) > 0) $res['status'] = true;
                         else $res['err'] = 'Add failed';
--- App/API/Controller/TestController.class.php
+++ App/API/Controller/TestController.class.php
@@ -1,50 +1,71 @@
 <?php
+/**
+ * 如何测试：
+ * 域名/根目录/API/Test/index/type/get/api/Category/func/basic
+ * type：get/add/update/delete，默认get
+ * api：API控制器，默认无
+ * func：API控制器里的方法，默认basic
+ *
+ * API参数在每个测试方法里自行修改
+ */
+
 namespace API\Controller;
 use Think\Controller;
 class TestController extends Controller {
-    public function index($type="get"){
+    public function index($type="get", $api="", $func="basic"){
         $api_url = C("API_URL");
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $api_url . "API/Category/basic");
+        curl_setopt($curl, CURLOPT_URL, $api_url . "API/$api/$func");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        if ($type == 'post') {
-            echo "post<br>";
-            curl_setopt($curl, CURLOPT_POST, 1);
+
+        if ($type == 'add') {
+            // API参数
             $data = array(
-                "name" => "食品",
+                "name" => "SISU",
+                "desc" => "老外的保健品品牌",
                 "lang" => "cn",
                 "api" => "add"
             );
+
+            echo "post<br>";
+            curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         } elseif ($type == 'delete') {
-            echo "delete<br>";
-            curl_setopt($curl, CURLOPT_POST, 1);
+            // API参数
             $data = array(
                 "id" => "2",
                 "lang" => "cn",
                 "api" => "delete"
             );
+
+            echo "delete<br>";
+            curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         } elseif ($type == 'update') {
-            echo "update<br>";
-            curl_setopt($curl, CURLOPT_POST, 1);
+            // API参数
             $data = array(
                 "id" => "3",
                 "name" => "首饰",
                 "lang" => "cn",
                 "api" => "update"
             );
+
+            echo "update<br>";
+            curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         } elseif ($type == 'get') {
-            echo "get<br>";
-            curl_setopt($curl, CURLOPT_POST, 1);
+            // API参数
             $data = array(
                 "id" => "",
                 "lang" => "cn",
                 "api" => "get"
             );
+
+            echo "get<br>";
+            curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         }
+
         $data = curl_exec($curl);
         curl_close($curl);
 
