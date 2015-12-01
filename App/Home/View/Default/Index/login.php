986b62b18585ed25b2284f2e8fc93b010439d9af
mrwaynewu <waynewjq@gmail.com>
2015-12-01 11:34:28 -0500

    img

---
+++ App/Home/View/Default/Index/login.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("wamp/wwww/pbplatform/Home/Index/login.html");
+
+print $_GET['username']; print '<br>';
+
+$username=$_REQUEST['username'];
+$password=$_REQUEST['password'];
+$remeberme=$_REQUEST['remeberme'];
+
+
+?>
