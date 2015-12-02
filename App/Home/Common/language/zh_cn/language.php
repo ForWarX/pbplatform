6aadd114b1b5d89032d733e9e24f648517b4efaa
mrwaynewu <waynewjq@gmail.com>
2015-12-02 13:52:27 -0500

    changes

---
+++ App/Home/Common/language/zh_cn/language.php
@@ -0,0 +1,16 @@
+<?php
+
+/* {$lang.login}*/
+
+/* 用户登录语言项 */
+$_LANG['index_username'] = "用户名";
+$_LANG['index_login'] = "登录";
+$_LANG['index_userpassword'] = "用户密码";
+$_LANG['index_remeberme'] = "记住我";
+
+
+/* 公共语言项 */
+
+
+
+?>
--- App/Home/View/Default/Index/language.php
+++ App/Home/View/Default/Index/language.php
@@ -12,18 +12,5 @@ function language(){
 }
 
 language();
-<!---->
-/* {$lang.login}*/
-
-/* 用户登录语言项 */
-$_LANG['index_username'] = "用户名";
-$_LANG['index_login'] = "登录";
-$_LANG['index_userpassword'] = "用户密码";
-$_LANG['index_remeberme'] = "记住我";
-
-
-/* 公共语言项 */
-
-
 
 ?>
