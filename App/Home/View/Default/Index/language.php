b63b5430121ed3f3735245988e69aa67a1a43131
polarbearit <it@pbcc.ca>
2015-12-03 18:08:45 -0500

    c

--- App/Home/View/Default/Index/language.php
+++ App/Home/View/Default/Index/language.php
@@ -13,4 +13,19 @@ function language(){
 
 language();
 
+
+$lang['lang_var1'] = 'en_us';
+$lang['lang_var2'] = 'zh_cn';
+//$lang['lang_var3'] = '语言定义3';
+L($lang);
+
+//获取语言变量
+$langVar = L('LANG_VAR');
+//如果参数为空，表示获取当前定义的全部语言变量（包括语言定义文件中的）：
+//$lang = L();
+
+
 ?>
+
+
+
