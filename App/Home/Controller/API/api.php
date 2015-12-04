6f217e3f8b54b0d2bbea1801ddf025f6d3454f77
polarbearit <it@pbcc.ca>
2015-12-04 09:59:07 -0500

    c

---
+++ App/Home/Controller/API/api.php
@@ -0,0 +1,15 @@
+    public function api_request($api=null, $post_data=array("api"=>"get"), $method="basic") {
+        if ($api == null) return;
+
+        $api_url = C("API_URL");
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $api_url . "API/$api/$method");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // post start
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+        // post end
+        $data = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($data, true);
+    }
--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -2,6 +2,8 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+//require_once('api.php'); 
+
     public function index(){
         $this->display();
     }
@@ -22,22 +24,5 @@ class IndexController extends Controller {
         $this->display();
     }
 
-    public function test() {
-        $api_url = C("API_URL");
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $api_url . "API/Index/rest");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        // post start
-        curl_setopt($curl, CURLOPT_POST, 1);
-        $post_data = array(
-            "username" => "admin",
-            "password" => "12345"
-        );
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-        // post end
-        $data = curl_exec($curl);
-        curl_close($curl);
-        header("Content-type: text/html; charset=utf-8");
-        print_r(json_decode($data, true));
-    }
+
 }
